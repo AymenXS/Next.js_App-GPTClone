77c40d3f94bdeb45bea7a7dc8552721a8d7d135e
Aymen <aymenghaloua@gmail.com>
2024-05-29 16:10:54 +0100

    Fix: Fixing Query.

--- utils/action.js
+++ utils/action.js
@@ -32,20 +32,17 @@ export const getExistingTour = async ({ city, country }) => {
 };
 
 export const generateTourResponse = async ({ city, country }) => {
-  const query = `Find a ${city} in this ${country}.
-    If ${city} in this ${country} exists, create a list of things families can do in this ${city}, ${country}.
-    Once you have a list, create a one-day tour. Response should be in the following JSON format:
-    {
-      "tour":{
-        "city": "${city}",
-        "country": "${country}",
-        "title": "title of the tour",
-        "description": "description of the city and tour",
-        "stops": ["short paragraph on the stop 1 ", "short paragraph on the stop 2", "short paragraph on the stop 3"]
-      }
-    }
-
-    If you can't find info on exact ${city}, or ${city} does not exist, or its population is less than 1, or it is not located in the following ${country} return { "tour": null }, with no additional characters.`;
+  const query = `Find a ${city} in ${country}. If ${city} in ${country} exists, create a list of things families can do in ${city}, ${country}. Once you have a list, create a one-day tour. Response should be in the following JSON format:
+  {
+   "tour": {
+    "city": "${city}",
+    "country": "${country}",
+    "title": "title of the tour",
+    "description": "description of the city and tour",
+    "stops": ["short paragraph on stop 1", "short paragraph on stop 2", "short paragraph on stop 3"]
+   }
+  }
+  If you can't find info on the exact ${city}, or ${city} does not exist, or its population is less than 1, or it is not located in ${country}, return { "tour": null }, with no additional characters.s`;
 
   try {
     const response = await openai.chat.completions.create({
