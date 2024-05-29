603ea446cbecce13f7ed8ad63e04275a3d56cb16
Aymen <aymenghaloua@gmail.com>
2024-05-29 15:49:38 +0100

    Feature: Adding more "Tours" functionality.

--- components/NewTour.jsx
+++ components/NewTour.jsx
@@ -17,11 +17,11 @@ const NewTour = () => {
 
       const newTour = await generateTourResponse(destination);
       if (newTour) {
-        await createNewTour(newTour);
+        const response = await createNewTour(newTour);
         queryClient.invalidateQueries({ queryKey: ['tours'] });
         return newTour;
       }
-      
+
       toast.error('No matching city found...');
       return null;
     },
